2232c772332d5955dd71b9fd6742d1c5b4fb7fcf
Sylvain Penaud <sylvainpenaud13@gmail.com>
2019-05-11 00:45:08 +0200

    nouvel algorithme

--- automate.py
+++ automate.py
@@ -4,15 +4,35 @@ import matplotlib.pyplot as plt
 
 participant_choice = input("Choisissez un nombre entre 0 et 100 : ")
 
-len_array = int(participant_choice)
+size_array = int(participant_choice)
 
-matrice = np.zeros((len_array), dtype = np.int16)
+line = np.zeros((size_array), dtype = np.int16)
 
-center =  matrice.size // 2
+center =  line.size // 2
 
-matrice[center] = 1
+line[center] = 1
 
-print(matrice)
+new_line = np.zeros((size_array), dtype = np.int16)
+
+print(line)
+
+iteration = 0
+
+while iteration < 5:
+    for i in range(1, len(line)- 1):
+        if line[i - 1] == 1 and line[i] == 1 and line[i + 1] == 0:
+            new_line[i] = 1
+        elif line[i - 1] == 1 and line[i] == 0 and line[i + 1] == 1:
+            new_line[i] = 1
+        elif line[i - 1] == 0 and line[i] == 1 and line[i + 1] == 1:
+            new_line[i] = 1
+        elif line[i - 1] == 0 and line[i] == 1 and line[i + 1] == 0:
+            new_line[i] = 1
+        elif line[i - 1] == 0 and line[i] == 0 and line[i + 1] == 1:
+            new_line[i] = 1
+    print(new_line)
+    iteration =+ 1
+    line = new_line
 
 #plt.imshow(matrice)
 #plt.show()
