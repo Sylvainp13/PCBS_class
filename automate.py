74efbeb569f4a5227a52c81b57a4f2f1d3c47421
Sylvain Penaud <sylvainpenaud13@gmail.com>
2019-06-17 23:31:40 +0200

    final_push

--- automate.py
+++ automate.py
@@ -1,124 +1,154 @@
+""" Implémentation d'un automate cellulaire à une dimension
+
+"""
+
 import numpy as np
-
 import matplotlib.pyplot as plt
+import webbrowser as wb
 
-rule_choice = input("Choisissez l'une de ces fameuses règles [28 | 50 | 60 | 90 | 110 | 146] : ")
+width = 500 # nombre d'éléments contenus dans la population initiale
+lenght = 500 # nonbre de générations
 
-row_number = input("Choisissez un nombre de générations entre 0 et 1000 : ")
+wb.open('https://fr.wikipedia.org/wiki/Automate_cellulaire') # ouvre une page wikipédia en lien avec les automates cellulaires
 
-col_number = input("Largeur : choisissez un nombre entre 0 et 1000 : ")
+print("Lisez attentivement les informations sur la page qui vient de s'afficher")
 
-#if int(rule_choice) != 28 or 50 or 60 or 90 or 110 or 146:
-#    rule_choice = input("Veuillez choisir l'un de ces nombres [28 | 50 | 60 | 90 | 110 | 146] : ")
-#if int(row_number) > 1000:
-#    row_number = input("Veuillez ne pas choisir un nombre supérieur à 1000 : ")
-#if int(col_number) > 1000:
-#    col_number = input("Veuillez ne pas choisir un nombre supérieur à 1000 : ")
+rule_choice = int(input("Maintenant que vous en savez plus à propos des automates cellulaires, choisissez le chiffre correspondant à l'une des règles proposée entre crochet -> [Règle n°28 | Règle n°30 | Règle n°50 | Règle n°60 | Règle n°110 | Règle n°146 | Règle n°184] : "))
 
-width = int(row_number)
+rule = rule_choice
 
-lenght = int(row_number)
+population = np.zeros((lenght), dtype = np.int32) # définit la 1ère population comme une suite de 0
 
-Rule = int(rule_choice)
+center =  population.size // 2
 
-pop = np.zeros((lenght), dtype = np.int32)
+population[center] = 1
 
-center =  pop.size // 2
+new_generation = np.zeros((lenght), dtype = np.int32)
 
-pop[center] = 1
+def rule_generator(population):
+    """ Transforme un array en un nouvel array de même longueur en fonction de la règle appliquée
 
-new_population = np.zeros((lenght), dtype = np.int32)
+    Argument :
+    population : un array d'une longueur de 500 éléments
 
-def Rule_maker(pop):
-    if Rule == 28:
-        new_population = np.zeros((len(pop)), dtype = np.int32)
-        for i in range(1, len(pop)-1):
-            if pop[i - 1] == 1 and pop[i] == 0 and pop[i + 1] == 0:
-                new_population[i] = 1
-            elif pop[i - 1] == 0 and pop[i] == 1 and pop[i + 1] == 1:
-                new_population[i] = 1
-            elif pop[i - 1] == 0 and pop[i] == 1 and pop[i + 1] == 0:
-                new_population[i] = 1
+    Retourne :
+    new_generation : un nouvel array de même longueur résultant de la transformation de la population initiale en fonction de la règle appliquée
+
+    """
+    if rule == 28:
+        new_generation = np.zeros((len(population)), dtype = np.int32)
+        for i in range(1, len(population)-1):
+            if population[i - 1] == 1 and population[i] == 0 and population[i + 1] == 0:
+                new_generation[i] = 1
+            elif population[i - 1] == 0 and population[i] == 1 and population[i + 1] == 1:
+                new_generation[i] = 1
+            elif population[i - 1] == 0 and population[i] == 1 and population[i + 1] == 0:
+                new_generation[i] = 1
             else:
-                new_population[i] = 0
-        return new_population
-    if Rule == 110:
-        new_population = np.zeros((len(pop)), dtype = np.int32)
-        for i in range(1, len(pop)-1):
-            if pop[i - 1] == 1 and pop[i] == 1 and pop[i + 1] == 1:
-                new_population[i] = 0
-            elif pop[i - 1] == 1 and pop[i] == 0 and pop[i + 1] == 0:
-                new_population[i] = 0
-            elif pop[i - 1] == 0 and pop[i] == 0 and pop[i + 1] == 0:
-                new_population[i] = 0
+                new_generation[i] = 0
+        return new_generation
+    if rule == 30:
+        new_generation = np.zeros((len(population)), dtype = np.int32)
+        for i in range(1, len(population)-1):
+            if population[i - 1] == 1 and population[i] == 0 and population[i + 1] == 0:
+                new_generation[i] = 1
+            elif population[i - 1] == 0 and population[i] == 1 and population[i + 1] == 1:
+                new_generation[i] = 1
+            elif population[i - 1] == 0 and population[i] == 1 and population[i + 1] == 0:
+                new_generation[i] = 1
+            elif population[i - 1] == 0 and population[i] == 0 and population[i + 1] == 1:
+                new_generation[i] = 1
             else:
-                new_population[i] = 1
-        return new_population
-    if Rule == 50:
-        new_population = np.zeros((len(pop)), dtype = np.int32)
-        for i in range(1, len(pop)-1):
-            if pop[i - 1] == 1 and pop[i] == 0 and pop[i + 1] == 1:
-                new_population[i] = 1
-            elif pop[i - 1] == 1 and pop[i] == 0 and pop[i + 1] == 0:
-                new_population[i] = 1
-            elif pop[i - 1] == 0 and pop[i] == 0 and pop[i + 1] == 1:
-                new_population[i] = 1
+                new_generation[i] = 0
+        return new_generation
+    if rule == 50:
+        new_generation = np.zeros((len(population)), dtype = np.int32)
+        for i in range(1, len(population)-1):
+            if population[i - 1] == 1 and population[i] == 0 and population[i + 1] == 1:
+                new_generation[i] = 1
+            elif population[i - 1] == 1 and population[i] == 0 and population[i + 1] == 0:
+                new_generation[i] = 1
+            elif population[i - 1] == 0 and population[i] == 0 and population[i + 1] == 1:
+                new_generation[i] = 1
             else:
-                new_population[i] = 0
-        return new_population
-    if Rule == 60:
-        new_population = np.zeros((len(pop)), dtype = np.int32)
-        for i in range(1, len(pop)-1):
-            if pop[i - 1] == 1 and pop[i] == 0 and pop[i + 1] == 1:
-                new_population[i] = 1
-            elif pop[i - 1] == 1 and pop[i] == 0 and pop[i + 1] == 0:
-                new_population[i] = 1
-            elif pop[i - 1] == 0 and pop[i] == 1 and pop[i + 1] == 1:
-                new_population[i] = 1
-            elif pop[i - 1] == 0 and pop[i] == 1 and pop[i + 1] == 0:
-                new_population[i] = 1
+                new_generation[i] = 0
+        return new_generation
+    if rule == 60:
+        new_generation = np.zeros((len(population)), dtype = np.int32)
+        for i in range(1, len(population)-1):
+            if population[i - 1] == 1 and population[i] == 0 and population[i + 1] == 1:
+                new_generation[i] = 1
+            elif population[i - 1] == 1 and population[i] == 0 and population[i + 1] == 0:
+                new_generation[i] = 1
+            elif population[i - 1] == 0 and population[i] == 1 and population[i + 1] == 1:
+                new_generation[i] = 1
+            elif population[i - 1] == 0 and population[i] == 1 and population[i + 1] == 0:
+                new_generation[i] = 1
             else:
-                new_population[i] = 0
-        return new_population
-    if Rule == 90:
-        new_population = np.zeros((len(pop)), dtype = np.int32)
-        for i in range(1, len(pop)-1):
-            if pop[i - 1] == 1 and pop[i] == 1 and pop[i + 1] == 0:
-                new_population[i] = 1
-            elif pop[i - 1] == 1 and pop[i] == 0 and pop[i + 1] == 0:
-                new_population[i] = 1
-            elif pop[i - 1] == 0 and pop[i] == 1 and pop[i + 1] == 1:
-                new_population[i] = 1
-            elif pop[i - 1] == 0 and pop[i] == 0 and pop[i + 1] == 1:
-                new_population[i] = 1
+                new_generation[i] = 0
+        return new_generation
+    if rule == 110:
+        new_generation = np.zeros((len(population)), dtype = np.int32)
+        for i in range(1, len(population)-1):
+            if population[i - 1] == 1 and population[i] == 1 and population[i + 1] == 1:
+                new_generation[i] = 0
+            elif population[i - 1] == 1 and population[i] == 0 and population[i + 1] == 0:
+                new_generation[i] = 0
+            elif population[i - 1] == 0 and population[i] == 0 and population[i + 1] == 0:
+                new_generation[i] = 0
             else:
-                new_population[i] = 0
-        return new_population
-    if Rule == 146:
-        new_population = np.zeros((len(pop)), dtype = np.int32)
-        for i in range(1, len(pop)-1):
-            if pop[i - 1] == 1 and pop[i] == 1 and pop[i + 1] == 1:
-                new_population[i] = 1
-            elif pop[i - 1] == 1 and pop[i] == 0 and pop[i + 1] == 0:
-                new_population[i] = 1
-            elif pop[i - 1] == 0 and pop[i] == 0 and pop[i + 1] == 1:
-                new_population[i] = 1
+                new_generation[i] = 1
+        return new_generation
+    if rule == 146:
+        new_generation = np.zeros((len(population)), dtype = np.int32)
+        for i in range(1, len(population)-1):
+            if population[i - 1] == 1 and population[i] == 1 and population[i + 1] == 1:
+                new_generation[i] = 1
+            elif population[i - 1] == 1 and population[i] == 0 and population[i + 1] == 0:
+                new_generation[i] = 1
+            elif population[i - 1] == 0 and population[i] == 0 and population[i + 1] == 1:
+                new_generation[i] = 1
             else:
-                new_population[i] = 0
-        return new_population
+                new_generation[i] = 0
+        return new_generation
+    if rule == 184:
+        new_generation = np.zeros((len(population)), dtype = np.int32)
+        for i in range(1, len(population)-1):
+            if population[i - 1] == 1 and population[i] == 1 and population[i + 1] == 1:
+                new_generation[i] = 1
+            elif population[i - 1] == 1 and population[i] == 0 and population[i + 1] == 1:
+                new_generation[i] = 1
+            elif population[i - 1] == 0 and population[i] == 1 and population[i + 1] == 1:
+                new_generation[i] = 1
+            elif population[i - 1] == 1 and population[i] == 0 and population[i + 1] == 0:
+                new_generation[i] = 1
+            else:
+                new_generation[i] = 0
+        return new_generation
 
-def create_board(n_rows, n_cols):
+def automate_generator(n_rows, n_cols):
+    """ Créé une matrice montrant l'évolution de la population en fonction de la règle appliquée
+
+    Argurment :
+    n_rows : un entier correspondant au nombre de génération de l'automate
+    n_cols : un entier correspondant au nombre d'éléments dans la population initiale
+
+    Retourne :
+    board : une matrice montrant l'évolution des conditions initiales en fonction de la règle appliquée
+    """
     start = np.zeros(n_cols, dtype = np.int32)
     start[n_cols // 2] = 1
     board = np.zeros((n_rows, n_cols), dtype = np.int32)
     board[0,:] = start
     for i in range(n_rows - 1):
-        current_pop = board[i , :]
-        board[i + 1, :] = Rule_maker(current_pop)
+        current_population = board[i , :]
+        board[i + 1, :] = rule_generator(current_population)
     return board
 
-automate = create_board(width, lenght)
+wb.open('http://mathworld.wolfram.com/Rule' + str(rule) + '.html')
 
-plt.imshow(automate, cmap = "hot")
+automate = automate_generator(width, lenght)
+
+plt.imshow(automate)
 plt.show()
-plt.imsave('Rule_110.png', automate, dpi = 200)
+plt.imsave('rule_' + str(rule) + '.png', automate, dpi = 100, cmap = "hot") # enregistre une image au au format .png
